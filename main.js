6f5f5b8d5902bc3bb15e7beb2beaafa8ae3f7ac8
msporich <48558105+msporich@users.noreply.github.com>
2019-12-08 20:37:48 -0500

    added google maps api

--- main.js
+++ main.js
@@ -75,3 +75,17 @@ function topDeals(jsonObject) {
 
 }
 
+/* Code for Google Maps api */
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var coords = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: coords, map: map});
+}
+
+
